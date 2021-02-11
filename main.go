9fd3d85247628cfffdfbbc4ae64a0fa8e9d39fa7
Will Shen <will.shen@anz.com>
2021-02-12 09:31:08 +1100

    setup skeleton structure for the options and flags for the cli

--- main.go
+++ main.go
@@ -6,7 +6,32 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
+var (
+	app = kingpin.New("Zendesk-Search", "Welcome to Zendesk Search!")
+
+	list      = app.Command("list", "List all possible fields in a table.")
+	listTable = list.Arg("table", "Name of the table.").Required().String()
+	listField = list.Arg("field", "Name of the field.").Required().String()
+
+	search      = app.Command("search", "Search a field in a table.")
+	searchTable = search.Arg("table", "Name of the table.").Required().String()
+	searchField = search.Arg("field", "Name of the field.").Required().String()
+)
+
 func main() {
-	app := kingpin.New("my-app", "My Example CLI Application With Bash Completion")
-	kingpin.MustParse(app.Parse(os.Args[1:]))
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	// Post message
+	case list.FullCommand():
+		println(*listTable)
+
+	// Register user
+	case search.FullCommand():
+		println(*searchTable)
+	}
+}
+
+// Configs contains the arguments parsed from command line
+type Config struct {
+	table string
+	field string
 }
